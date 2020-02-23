f7fc9503c8d44dca9bd84c3d8330ebe55754b50c
vladislav <bincha.1997@gmail.com>
2020-02-23 23:33:30 +0900

    Added handler form

--- section2/js/script.js
+++ section2/js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', function() {
   'use strict';
 
   let tab = document.querySelectorAll('.info-header-tab'),
@@ -23,7 +23,7 @@ window.addEventListener('DOMContentLoaded', function () {
   }
   showTabContent(0);
 
-  info.addEventListener('click', function (event) {
+  info.addEventListener('click', function(event) {
     let target = event.target;
     if (target && target.classList.contains('info-header-tab')) {
       for (let i = 0; i < tab.length; i++) {
@@ -48,7 +48,7 @@ window.addEventListener('DOMContentLoaded', function () {
       total,
       hours,
       minutes,
-      seconds
+      seconds,
     };
   }
 
@@ -61,7 +61,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     function updateClock() {
       let t = getTimeRemaining(endtime);
-      
+
       function addZero(num) {
         if (num <= 9) {
           return '0' + num;
@@ -88,18 +88,63 @@ window.addEventListener('DOMContentLoaded', function () {
     let overlay = document.querySelector('.overlay');
     let close = document.querySelector('.popup-close');
 
-    element.addEventListener('click', function () {
+    element.addEventListener('click', function() {
       overlay.style.display = 'block';
       this.classList.add('more-splash');
       document.body.style.overflow = 'hidden';
     });
 
-    close.addEventListener('click', function () {
+    close.addEventListener('click', function() {
       overlay.style.display = 'none';
       element.classList.remove('more-splash');
       document.body.style.overflow = '';
     });
   }
-
   modalWindow(more);
+
+  //form
+  let message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся!',
+    failure: 'Что-то пошло не так...',
+  };
+
+  let form = document.querySelector('.main-form'),
+    input = form.getElementsByTagName('input'),
+    statusMessage = document.createElement('div');
+
+  statusMessage.classList.add('status');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    form.appendChild(statusMessage);
+
+    let request = new XMLHttpRequest();
+    request.open('POST', 'server.php');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+    let formData = new FormData(form);
+
+    let obj = {};
+    formData.forEach(function(value, key) {
+      obj[key] = value;
+    });
+    let json = JSON.stringify(obj);
+
+    request.send(json);
+
+    request.addEventListener('readystatechange', function() {
+      if (request.readyState < 4) {
+        statusMessage.innerHTML = message.loading;
+      } else if (request.readyState === 4 && request.status == 200) {
+        statusMessage.innerHTML = message.success;
+      } else {
+        statusMessage.innerHTML = message.failure;
+      }
+    });
+
+    for (let i = 0; i < input.length; i++) {
+      input[i].value = '';
+    }
+  });
 });
